2c695f0e742a7f29486a5814796f22f5ad6aac65
guimy <guimingyue@gmail.com>
2022-01-22 23:32:29 +0800

    finished the part 2: World's Simplest SQL Compiler and Virtual Machine

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
 use std::io;
 use std::process;
+use crate::PrepareResult::{PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT};
+
+#[derive(PartialEq)]
+pub enum MetaCommandResult {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+}
+
+#[derive(PartialEq)]
+pub enum PrepareResult {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+}
+
+#[derive(PartialEq)]
+pub enum StatementType {
+    STATEMENT_INSERT,
+    STATEMENT_SELECT,
+    STATEMENT_UNSUPPORTED
+}
+
+pub struct Statement {
+    stmt_type: StatementType
+}
 
 fn main() {
     fn print_prompt() {
@@ -17,13 +41,65 @@ fn main() {
         String::from(input_buffer.trim())
     }
 
+    fn do_meta_command(command: &str) -> MetaCommandResult {
+        if command.eq(".exit") {
+            process::exit(0x0100);
+        }
+        MetaCommandResult::META_COMMAND_UNRECOGNIZED_COMMAND
+    }
+
+    fn prepare_statement(command: &str) -> (Box<Statement>, PrepareResult) {
+        let stmt_type= if command.starts_with("insert") {
+            StatementType::STATEMENT_INSERT
+        } else if command.starts_with("select") {
+            StatementType::STATEMENT_SELECT
+        } else {
+            StatementType::STATEMENT_UNSUPPORTED
+        };
+
+        let stmt = Statement {
+            stmt_type
+        };
+
+        if stmt.stmt_type == StatementType::STATEMENT_UNSUPPORTED {
+            (Box::new(stmt), PREPARE_UNRECOGNIZED_STATEMENT)
+        } else {
+            (Box::new(stmt), PREPARE_SUCCESS)
+        }
+    }
+
+    fn execute_statement(stmt: &Statement) {
+        match &stmt.stmt_type {
+            StatementType::STATEMENT_INSERT => {
+                println!("This is where we would do an insert")
+            },
+            StatementType::STATEMENT_SELECT => {
+                println!("This is where we would do an select")
+            },
+            _ => println!("error")
+        }
+    }
+
     loop {
         print_prompt();
         let command = read_input();
-        if command.eq(".exit") {
-            process::exit(0x0100);
-        } else {
-            println!("Unrecognized command {}", command);
+        if command.starts_with(".") {
+            let meta_result = do_meta_command(&command);
+            match meta_result {
+                MetaCommandResult::META_COMMAND_UNRECOGNIZED_COMMAND => {
+                    println!("Unrecognized command {}", command);
+                    continue;
+                },
+                MetaCommandResult::META_COMMAND_SUCCESS => continue
+            }
         }
+
+        let (stmt, prepare_result) = prepare_statement(&command);
+        if prepare_result == PREPARE_UNRECOGNIZED_STATEMENT {
+            println!("Unrecognized keyword at start of {}.", command);
+            continue;
+        }
+        execute_statement(&stmt);
+        println!("Executed.");
     }
 }
