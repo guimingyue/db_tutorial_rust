6ae83f6d26d923aaaaf88a4b72130ae2552e39ba
guimy <guimingyue@gmail.com>
2022-01-22 22:43:18 +0800

    finished the part 1: Introduction and Setting up the REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    fn print_prompt() {
+        print!("db > ");
+    }
+
+    fn read_input() -> String {
+        let mut input_buffer = String::new();
+        let bytes_read =  io::stdin()
+            .read_line(&mut input_buffer)
+            .expect("Failed to read line");
+        if bytes_read < 0 {
+            panic!("Error reading input")
+        }
+        String::from(input_buffer.trim())
+    }
+
+    loop {
+        print_prompt();
+        let command = read_input();
+        if command.eq(".exit") {
+            process::exit(0x0100);
+        } else {
+            println!("Unrecognized command {}", command);
+        }
+    }
 }
